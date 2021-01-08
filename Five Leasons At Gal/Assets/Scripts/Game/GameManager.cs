ad146545d837258a7e7f339543e74311cd08bdc8
EditsOmega <titi123k@gmail.com>
2021-01-09 01:44:29 +0200

    camera switching
    check scene 1 for an example

---
+++ Five Leasons At Gal/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] gameCams;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        GetCameras();
+    }
+    public GameObject[] GetCameras()
+    {
+
+       
+        gameCams = GameObject.FindGameObjectsWithTag("GameCam");
+        // Sort That Array PLS its 1 am
+        //int counter = 1;
+        //while (counter != 0)
+        //{
+        //    for (int i = 1; i < gameCams.Length; i++)
+        //    {
+                
+        //        if ((int)gameCams[i].name[0] > (int)gameCams[i - 1].name[0])
+        //        {
+        //            counter++;
+        //            GameObject temp = gameCams[i];
+        //            gameCams[i] = gameCams[i - 1];
+        //            gameCams[i - 1] = temp;
+        //        }
+        //    }
+        //    counter = 0;
+        //}
+       
+        return gameCams;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Five Leasons At Gal/Assets/Scripts/Player/Camera/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public GameObject[] gameCams;
+    // Start is called before the first frame update
+    public GameObject currentCam;
+    private int camId;
+    void Awake()
+    {
+        
+        gameCams = GetComponent<GameManager>().GetCameras();
+        for (int i = 0; i < gameCams.Length; i++)
+        {
+            if(gameCams[i].name == currentCam.name)
+            {
+                camId = i;
+            }
+        }
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Input.inputString;
+
+        switch (input)
+        {
+            default:
+                Debug.Log(camId);
+                break;
+            case "d":
+            case "D":
+                if (camId < gameCams.Length - 1)
+                {
+                    gameCams[camId].GetComponent<Camera>().enabled = false;
+                    gameCams[camId + 1].GetComponent<Camera>().enabled = true;
+                    camId += 1;
+                    Debug.Log("right");
+                }
+                break;
+            case "a":
+            case "A":
+
+                if (camId > 0)
+                {
+                    gameCams[camId].GetComponent<Camera>().enabled = false;
+                    gameCams[camId - 1].GetComponent<Camera>().enabled = true;
+                    camId -= 1;
+                    Debug.Log("left");
+                }
+                break;
+        }
+       
+       
+    }
+}
